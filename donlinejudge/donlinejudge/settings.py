e7efead260102a8d60c176dba7a76f9070b89d94
NDThong <65402387+thongnd0200@users.noreply.github.com>
2021-05-16 14:33:13 +0700

    add static/images to upload images

--- donlinejudge/donlinejudge/settings.py
+++ donlinejudge/donlinejudge/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -140,6 +141,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # ==
 AUTH_USER_MODEL = 'accounts.User'
--- donlinejudge/donlinejudge/urls.py
+++ donlinejudge/donlinejudge/urls.py
@@ -1,4 +1,6 @@
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -9,3 +11,5 @@ urlpatterns = [
     path('', include('problem.urls')),
     path('', include('submission.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
