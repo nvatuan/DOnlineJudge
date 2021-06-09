f43cd0c0bf87c46993e8daf55e9bf0b0d16bc88a
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-06-09 17:43:28 +0700

    Update Dashboard.js

--- src/admin/Dashboard/Dashboard.js
+++ src/admin/Dashboard/Dashboard.js
@@ -5,8 +5,7 @@ import './Dashboard.scss';
 function Dashboard(props) {
     const [name, SetName] = useState('admin');
     const [role, Setrole] = useState('Super Admin');
-    const [image, setImage] = useState('http://nvatazure.southeastasia.cloudapp.azure.com/public/avatar/default.png')
-
+    const [image, setImage] = useState('https://www.pngitem.com/pimgs/m/128-1280822_check-mark-box-clip-art-blue-admin-icon.png')
     return (
         <div className="admin-content">
             <AdminNavbar />
