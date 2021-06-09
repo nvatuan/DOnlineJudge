845cf136c33a9ba0b6c5210bdc4f0f4b2d6b32ac
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-06-09 17:43:35 +0700

    Update CreateProblem.js

--- src/admin/Problems/CreateProblem/CreateProblem.js
+++ src/admin/Problems/CreateProblem/CreateProblem.js
@@ -1,152 +1,209 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ListGroup, Button, Form } from 'react-bootstrap';
 import Editor from '../../editor/Editor';
 import Swit from '../../switch/Swit';
 import './Createproblem.scss';
 import AdminNavbar from '../../AdminNavbar';
 import Sidebar from '../../Sidebar';
-import Sample from '../../Sample/Sample'
+import { useForm } from 'react-hook-form';
+
 function Createproblem(props) {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    }
+    const [fields, setFields] = useState([{ value: null }]);
+
+    function handleChange(i, event) {
+        const values = [...fields];
+        values[i].value = event.target.value;
+        setFields(values);
+    }
+
+    function handleAdd() {
+        const values = [...fields];
+        values.push({ value: null });
+        setFields(values);
+    }
+
+    function handleRemove(i) {
+        const values = [...fields];
+        values.splice(i, 1);
+        setFields(values);
+    }
     return (
         <div className="Edit-container">
             <AdminNavbar />
             <Sidebar />
             <div className="cp">
-                <ListGroup>
-                    <ListGroup.Item className="cp-header cp-list" >
-                        <h3>Add problem</h3>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="cp-list">
-                        <table>
-                            <tr>
-                                <td className="td-dis">
-                                    <i style={{ color: 'red' }}>*</i>
-                                    <span> Display ID</span> <br /> <br />
-                                    <input type="text" autocomplete="off" placeholder="Display ID" className="cp-displayid" />
-                                </td>
-                                <td className="td-til">
-                                    <i style={{ color: 'red' }}>*</i>
-                                    <span> Title</span> <br /> <br />
-                                    <input type="text" autocomplete="off" placeholder="Title" className="cp-title" />
-                                </td>
-                            </tr>
-                        </table>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <ListGroup>
+                        <ListGroup.Item className="cp-header cp-list" >
+                            <h3>Add problem</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="cp-list">
+                            <table>
+                                <tr>
+                                    <td className="td-dis">
+                                        <i style={{ color: 'red' }}>*</i>
+                                        <span> Display ID</span> <br /> <br />
+                                        <input type="text" autocomplete="off" placeholder="Display ID" className="cp-displayid" {...register("displayID")} required />
+                                    </td>
+                                    <td className="td-til">
+                                        <i style={{ color: 'red' }}>*</i>
+                                        <span> Title</span> <br /> <br />
+                                        <input type="text" autocomplete="off" placeholder="Title" className="cp-title" {...register("title")} required />
+                                    </td>
+                                </tr>
+                            </table>
 
-                    </ListGroup.Item>
-                    <ListGroup.Item className="cp-list">
-                        <i style={{ color: 'red' }}>*</i>
-                        <span> Description</span> <br /> <br />
-                        <Editor></Editor>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="cp-list">
-                        <i style={{ color: 'red' }}>*</i>
-                        <span> Input Description</span> <br /> <br />
-                        <Editor></Editor>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="cp-list">
-                        <i style={{ color: 'red' }}>*</i>
-                        <span> Output Description</span> <br /> <br />
-                        <Editor></Editor>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="cp-list">
-                        <table>
-                            <tr>
-                                <td className="td-time">
-                                    <i style={{ color: 'red' }}>*</i>
-                                    <span> Time Limit (ms)</span> <br /> <br />
-                                    <input type="Number" autocomplete="off" placeholder="Time Limit" className="cp-time" />
-                                </td>
-                                <td className="td-mem">
-                                    <i style={{ color: 'red' }}>*</i>
-                                    <span> Memory limit (MB)</span> <br /> <br />
-                                    <input type="Number" autocomplete="off" placeholder="Memory limit (MB)" className="cp-mem" />
-                                </td>
-                                <td className="td-dif">
-                                    <i style={{ color: 'red' }}>*</i>
-                                    <span> Difficult</span> <br /> <br />
-                                    <select name="Difficulty" className="cp-dif">
-                                        <option value="Low">Low</option>
-                                        <option value="Mid">Mid</option>
-                                        <option value="High">High</option>
-                                    </select>
-                                </td>
-                            </tr>
-                            <br />
-                            <tr>
-                                <td>
-                                    <span> Visible</span> <br /> <br />
-                                    <Swit></Swit></td>
-                                <td>
-                                    <i style={{ color: 'red' }}>*</i>
-                                    <span> Language</span> <br /> <br />
-                                    <input
-                                        onChange="{handleLanguageChange}"
-                                        type="checkbox"
-                                        name="Language"
-                                        value="C"
-                                    />C
-                                <input
-                                        onChange="{handleLanguageChange}"
-                                        type="checkbox"
-                                        name="Language"
-                                        value="C++"
-                                    />C++
-                                <input
-                                        onChange="{handleLanguageChange}"
-                                        type="checkbox"
-                                        name="Language"
-                                        value="Java"
-                                    />Java
-                                 <input
-                                        onChange="{handleLanguageChange}"
-                                        type="checkbox"
-                                        name="Language"
-                                        value="Python"
-                                    />Python
-                            </td>
-
-                            </tr>
-                        </table>
-
-                    </ListGroup.Item>
-                    <ListGroup.Item className="cp-list">
-                        <i style={{ color: 'red' }}>*</i>
-                        <span> Sample</span> <br /> <br />
-                        <Sample></Sample>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="cp-list">
-                        <span> Hint</span> <br /> <br />
-                        <Editor></Editor>
-                    </ListGroup.Item >
-                    <ListGroup.Item className="cp-list">
-                        <table>
-                            <tr>
-                                <td className="td-type">
-                                    <span> Type</span> <br /> <br />
-                                    <input type="radio" name="type" value="ACM" />ACM
+                        </ListGroup.Item>
+                        <ListGroup.Item className="cp-list">
+                            <i style={{ color: 'red' }}>*</i>
+                            <span> Description</span> <br /> <br />
+                            <Form.Control as="textarea" rows={10} cols={150} {...register("description")} required>
+                            </Form.Control>
+                            {/* <Editor></Editor> */}
+                        </ListGroup.Item>
+                        <ListGroup.Item className="cp-list">
+                            <i style={{ color: 'red' }}>*</i>
+                            <span> Input Description</span> <br /> <br />
+                            <Form.Control as="textarea" rows={10} cols={150} {...register("inputDescription")} required>
+                            </Form.Control>
+                            {/* <Editor></Editor> */}
+                        </ListGroup.Item>
+                        <ListGroup.Item className="cp-list">
+                            <i style={{ color: 'red' }}>*</i>
+                            <span> Output Description</span> <br /> <br />
+                            <Form.Control as="textarea" rows={10} cols={150} {...register("outputDescription")} required>
+                            </Form.Control>
+                            {/* <Editor></Editor> */}
+                        </ListGroup.Item>
+                        <ListGroup.Item className="cp-list">
+                            <table>
+                                <tr>
+                                    <td className="td-time">
+                                        <i style={{ color: 'red' }}>*</i>
+                                        <span> Time Limit (ms)</span> <br /> <br />
+                                        <input type="Number" autocomplete="off" placeholder="Time Limit" className="cp-time" {...register("timeLimit")} required />
+                                    </td>
+                                    <td className="td-mem">
+                                        <i style={{ color: 'red' }}>*</i>
+                                        <span> Memory limit (MB)</span> <br /> <br />
+                                        <input type="Number" autocomplete="off" placeholder="Memory limit (MB)" className="cp-mem" {...register("memoryLimit")} required />
+                                    </td>
+                                    <td className="td-dif">
+                                        <i style={{ color: 'red' }}>*</i>
+                                        <span> Difficult</span> <br /> <br />
+                                        <select name="Difficulty" className="cp-dif" {...register("difficult")}>
+                                            <option value="Low">Low</option>
+                                            <option value="Mid">Mid</option>
+                                            <option value="High">High</option>
+                                        </select>
+                                    </td>
+                                </tr>
                                 <br />
-                                    <input inline type="radio" name="type" value="IO" />IO
-                            </td>
-                                <td className="td-test ">
-                                    <span> Test Case</span> <br /> <br />
-                                    <input type="file" />
+                                <tr>
+                                    <td>
+                                        <span> Visible</span> <br /> <br />
+                                        <Swit {...register("visible")} ></Swit></td>
+                                    <td>
+                                        <i style={{ color: 'red' }}>*</i>
+                                        <span> Language</span> <br /> <br />
+                                        <input className="cp-language"
+                                            onChange="{handleLanguageChange}"
+                                            type="checkbox"
+                                            name="Language"
+                                            value="C"
+                                            {...register("languages")}
+                                        />C
+                                         <input className="cp-language"
+                                            onChange="{handleLanguageChange}"
+                                            type="checkbox"
+                                            name="Language"
+                                            value="C++"
+                                            {...register("languages")}
+                                        />C++
+                                        <input className="cp-language"
+                                            onChange="{handleLanguageChange}"
+                                            type="checkbox"
+                                            name="Language"
+                                            value="Java"
+                                            {...register("languages")}
+                                        />Java
+                                         <input className="cp-language"
+                                            onChange="{handleLanguageChange}"
+                                            type="checkbox"
+                                            name="Language"
+                                            value="Python"
+                                            {...register("languages")}
+                                        />Python
+                                    </td>
 
-                                </td>
-                                <td className="td-io">
-                                    <span> IO mode</span> <br /> <br />
-                                    <input type="radio" name="io" value="standard" />Standard IO
-                                <br />
-                                    <input inline type="radio" name="io" value="file" />File IO
-                            </td>
-                            </tr>
-                        </table>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="cp-list">
-                        <Button className="cp-btn" variant="save">Save</Button>{' '}
-                    </ListGroup.Item>
+                                </tr>
+                            </table>
 
+                        </ListGroup.Item>
+                        <ListGroup.Item className="cp-list">
+                            <i style={{ color: 'red' }}>*</i>
+                            <span> Sample</span> <br /> <br />
+                            <div className="Sample" >
+                                {fields.map((field, idx) => {
+                                    return (
+                                        <div key={`${field}-${idx}`}>
+                                            <textarea
+                                                {...register("sample" + idx)}
+                                                className="textArea"
+                                                placeholder="Enter sample..."
+                                                value={field.value || ""}
+                                                required
+                                                onChange={e => handleChange(idx, e)}
 
-                </ListGroup>
+                                            />
+                                            <Button className="removeButton" onClick={() => handleRemove(idx)}>
+                                                X
+                                            </Button>
+                                        </div>
+                                    );
+                                })}
+                                <Button className="addButton" onClick={() => handleAdd()}>
+                                    +
+                                </Button>
+                            </div>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="cp-list">
+                            <span> Hint</span> <br /> <br />
+                            <Form.Control as="textarea" rows={10} cols={150} {...register("hint")}>
+                            </Form.Control>
+                            {/* <Editor></Editor> */}
+                        </ListGroup.Item >
+                        <ListGroup.Item className="cp-list">
+                            <table>
+                                <tr>
+                                    <td className="td-type">
+                                        <span> Type</span> <br /> <br />
+                                        <input type="radio" name="type" value="ACM" {...register("type")} /> ACM <br />
+                                        <input type="radio" name="type" value="IO" {...register("type")} /> IO
+                                    </td>
+                                    <td className="td-test ">
+                                        <span> Test Case</span> <br /> <br />
+                                        <input type="file" {...register("testCase")} />
+
+                                    </td>
+                                    <td className="td-io">
+                                        <span> IO mode</span> <br /> <br />
+                                        <input type="radio" name="io" value="standard" {...register("ioMode")} /> Standard IO <br />
+                                        <input type="radio" name="io" value="file" {...register("ioMode")} /> File IO
+                                    </td>
+                                </tr>
+                            </table>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="cp-list">
+                            <Button type="submit" className="cp-btn" variant="save">Save</Button>{' '}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Form>
+
             </div >
         </div>
     );
