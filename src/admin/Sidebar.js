c734e566dfdd7ddad8349aea1b6fe1b395e550d5
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-06-10 18:43:37 +0700

    Update Sidebar.js

--- src/admin/Sidebar.js
+++ src/admin/Sidebar.js
@@ -3,7 +3,7 @@ import 'react-pro-sidebar/dist/css/styles.css';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Sidebar.scss';
-import { AiFillAppstore, AiOutlineBars, AiOutlineCode} from 'react-icons/ai';
+import { AiFillAppstore, AiOutlineBars, AiOutlineCode } from 'react-icons/ai';
 import Logo from './Logo.jpg';
 function Sidebar(props) {
   return (
@@ -14,32 +14,36 @@ function Sidebar(props) {
         </div>
         <Menu iconShape="circle" >
           <MenuItem icon={<AiOutlineCode />}>
-              Dashboard
-              <Link to='/admin/dashboard'/>
-            </MenuItem>
-          <SubMenu title="General" icon={<AiFillAppstore/>}>
+            Dashboard
+              <Link to='/admin/dashboard' />
+          </MenuItem>
+          <SubMenu title="General" icon={<AiFillAppstore />}>
             <MenuItem>
               Announcement
                <Link to="/admin/announcement" />
             </MenuItem>
             <MenuItem >
               User
+<<<<<<< Updated upstream
               <Link to="/admin/users"/>
+=======
+              <Link to="/admin/user" />
+>>>>>>> Stashed changes
             </MenuItem>
           </SubMenu>
-          <SubMenu title="Problem" icon={<AiOutlineBars/>}>
+          <SubMenu title="Problem" icon={<AiOutlineBars />}>
             <MenuItem >
               Problem List
-              <Link to="/admin/problems"/>
+              <Link to="/admin/problem" />
             </MenuItem>
             <MenuItem>
               Create Problem
-              <Link to='/admin/problem/create'/>
+              <Link to='/admin/problem/create' />
             </MenuItem>
           </SubMenu>
         </Menu>
       </ProSidebar>
-      
+
     </div>
   )
 }
