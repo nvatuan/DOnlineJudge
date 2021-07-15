e70dfbd4ff9f08bbe65f08c366c33cd21b96a8fc
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-07-15 13:56:19 +0700

    Update Dashboard.js

--- src/admin/Dashboard/Dashboard.js
+++ src/admin/Dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import AdminNavbar from '../AdminNavbar';
 import Sidebar from '../Sidebar';
 import './Dashboard.scss';
+import { AiOutlineUser, AiOutlineLogin, AiOutlineCode, AiOutlineDashboard } from "react-icons/ai";
+import { RiAdminLine } from "react-icons/ri";
 function Dashboard(props) {
 
     return (
@@ -9,24 +11,33 @@ function Dashboard(props) {
             <AdminNavbar />
             <Sidebar />
             <div className="admin-dashboard">
+                <h2 className="dashboard__title"> <AiOutlineDashboard className="dashboard__title-icon"></AiOutlineDashboard> Dashboard</h2>
                 <div className="dashboard__list">
                     <div className="dashboard__item dashboard__item--admin-name">
-                        <div className="item__icon item__icon--first"></div>
+                        <div className="item__icon item__icon--first">
+                            <AiOutlineUser className="item__icon-font"></AiOutlineUser>
+                        </div>
                         <div className="item__title">Admin Name</div>
                         <div className="item__content">{"admin"}</div>
                     </div>
                     <div className="dashboard__item dashboard__item--admin-type">
-                        <div className="item__icon item__icon--second"></div>
+                        <div className="item__icon item__icon--second">
+                            <RiAdminLine className="item__icon-font"></RiAdminLine>
+                        </div>
                         <div className="item__title">Amin Type</div>
                         <div className="item__content">{"Regular"}</div>
                     </div>
                     <div className="dashboard__item dashboard__item--last-login">
-                        <div className="item__icon item__icon--third"></div>
+                        <div className="item__icon item__icon--third">
+                            <AiOutlineLogin className="item__icon-font"></AiOutlineLogin>
+                        </div>
                         <div className="item__title">Last Login</div>
                         <div className="item__content"></div>
                     </div>
                     <div className="dashboard__item dashboard__item--problems">
-                        <div className="item__icon item__icon--fourth"></div>
+                        <div className="item__icon item__icon--fourth">
+                            <AiOutlineCode className="item__icon-font"></AiOutlineCode>
+                        </div>
                         <div className="item__title">Problems</div>
                         <div className="item__content"></div>
                     </div>
