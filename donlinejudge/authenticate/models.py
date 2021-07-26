ee244dbca5f70cb75bc92cd623ab4c0bca5a068e
nvatuan <24392632+nvatuan@users.noreply.github.com>
2021-07-26 18:14:35 +0700

    Revert "Reformat return object"
    This reverts commit 6e896e6c13d5605a70231cf601e26632d13f42f0.

--- donlinejudge/authenticate/models.py
+++ donlinejudge/authenticate/models.py
@@ -1,14 +1,10 @@
 from rest_framework.authentication import TokenAuthentication, get_authorization_header
 from rest_framework.exceptions import AuthenticationFailed, AuthenticationFailed
-from rest_framework.response import Response
-from rest_framework import status
 
 from donlinejudge.settings import TOKEN_EXPIRE_AFTER_SECONDS
 from utils.time import utc_now
 
 from datetime import timedelta
-
-def response_unauthorized(message):
 
 class ExpiringTokenAuthentication(TokenAuthentication):
     def authenticate_credentials(self, key):
@@ -17,26 +13,14 @@ class ExpiringTokenAuthentication(TokenAuthentication):
         try:
             token = model.objects.get(key=key)
         except model.DoesNotExist:
-            resp = {
-                "data": "Your Auth token has expired"
-                "error": "Permission denied - Token expired"
-            }
-            return Response(resp, status=status.HTTP_404_NOT_FOUND)
+            raise AuthenticationFailed('Invalid token')
 
         if not token.user.is_active:
-            resp = {
-                "data": "This user is currently not active or has been deleted"
-                "error": "Permission denied - User inactive"
-            }
-            return Response(resp, status=status.HTTP_401_UNAUTHORIZED)
+            raise AuthenticationFailed('User inactive or deleted')
 
         # This is required for the time comparison
 
         if token.created < utc_now() - timedelta(seconds=TOKEN_EXPIRE_AFTER_SECONDS):
-            resp = {
-                "data": "Your Auth token has expired"
-                "error": "Permission denied - Token expired"
-            }
-            return Response(resp, status=status.HTTP_401_UNAUTHORIZED)
+            raise AuthenticationFailed('Token has expired')
 
         return token.user, token
