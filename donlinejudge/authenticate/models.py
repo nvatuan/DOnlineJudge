6e896e6c13d5605a70231cf601e26632d13f42f0
nvatuan <24392632+nvatuan@users.noreply.github.com>
2021-07-26 17:51:03 +0700

    Reformat return object

--- donlinejudge/authenticate/models.py
+++ donlinejudge/authenticate/models.py
@@ -1,10 +1,14 @@
 from rest_framework.authentication import TokenAuthentication, get_authorization_header
 from rest_framework.exceptions import AuthenticationFailed, AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework import status
 
 from donlinejudge.settings import TOKEN_EXPIRE_AFTER_SECONDS
 from utils.time import utc_now
 
 from datetime import timedelta
+
+def response_unauthorized(message):
 
 class ExpiringTokenAuthentication(TokenAuthentication):
     def authenticate_credentials(self, key):
@@ -13,14 +17,26 @@ class ExpiringTokenAuthentication(TokenAuthentication):
         try:
             token = model.objects.get(key=key)
         except model.DoesNotExist:
-            raise AuthenticationFailed('Invalid token')
+            resp = {
+                "data": "Your Auth token has expired"
+                "error": "Permission denied - Token expired"
+            }
+            return Response(resp, status=status.HTTP_404_NOT_FOUND)
 
         if not token.user.is_active:
-            raise AuthenticationFailed('User inactive or deleted')
+            resp = {
+                "data": "This user is currently not active or has been deleted"
+                "error": "Permission denied - User inactive"
+            }
+            return Response(resp, status=status.HTTP_401_UNAUTHORIZED)
 
         # This is required for the time comparison
 
         if token.created < utc_now() - timedelta(seconds=TOKEN_EXPIRE_AFTER_SECONDS):
-            raise AuthenticationFailed('Token has expired')
+            resp = {
+                "data": "Your Auth token has expired"
+                "error": "Permission denied - Token expired"
+            }
+            return Response(resp, status=status.HTTP_401_UNAUTHORIZED)
 
         return token.user, token
